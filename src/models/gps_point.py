024929925aa7e4ab77187e5ebadcc0e047fcb93e
Jonas <j.waid@mci4me.at>
2026-07-11 21:54:38 +0200

    feat: GPSPoint grundstruktur erstellt

---
+++ src/models/gps_point.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+class GPSPoint:
+    """Repräsentiert einen einzelnen GPS-Messpunkt."""
+
+    def __init__(self, lat: float, lon: float, ele: float, time: datetime, temp: float):
+        # _ im UML-Diagramm bedeutet "private". 
+
+        self._lat = lat
+        self._lon = lon
+        self._ele = ele
+        self._time = time
+        self._temp = temp
+
+    def get_distance_to(self, other: 'GPSPoint') -> float:
+        """
+        Berechnet die distanz (m) von diesem punkt zu einem anderen GPSPoint.
+        """
+        # GeoUtils aufrufen
+        pass
+        
+    def get_time_difference_to(self, other: 'GPSPoint') -> float:
+        """
+        Gibt die Zeitdifferenz in sec. zu einem anderen punkt zurück.
+        """
+        pass
+
+    # "Getter", falls ich die Werte mal abfragen möchte
+    @property
+    def lat(self) -> float:
+        return self._lat
+
+    @property
+    def lon(self) -> float:
+        return self._lon
+    
+    @property
+    def ele(self) -> float:
+        return self._ele
