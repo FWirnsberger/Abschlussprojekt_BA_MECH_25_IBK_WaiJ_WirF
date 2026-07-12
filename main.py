2827d084135521824bbbbd3b2bb5229c03423a85
Jonas <j.waid@mci4me.at>
2026-07-12 17:22:05 +0200

    feat: class GPSPoint fertig gemacht

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
+from datetime import datetime
 from src.calculations.route_data_kinematics import RouteData
+from src.models.gps_point import GPSPoint
 
 logging.basicConfig(format="%(asctime)s:%(levelname)s: %(message)s",
                     level=logging.INFO,)
@@ -17,6 +19,39 @@ def main():
     
     # Test ob Daten richtig sind
     print(route.data[['time', 'distance_m', 'speed_m_s', 'acceleration_m_s2', 'slope']].head())
+
+    logging.info("Starte Programm...\n")
+    
+    # --- TESTBEREICH FÜR GPS POINT ---
+    print("--- Teste GPSPoint Klasse ---")
+    
+    # Punkt 1 (Zeile 1 aus deiner CSV)
+    punkt_A = GPSPoint(
+        lat=47.583114, 
+        lon=12.170826, 
+        ele=494.88, 
+        time=datetime(2024, 8, 23, 16, 21, 14), 
+        temp=28.7
+    )
+    
+    # Punkt 2 (Zeile 2 aus deiner CSV)
+    punkt_B = GPSPoint(
+        lat=47.582910, 
+        lon=12.170766, 
+        ele=494.88, 
+        time=datetime(2024, 8, 23, 16, 21, 16), 
+        temp=28.8
+    )
+    
+    # Jetzt lassen wir die Objekte die Arbeit machen:
+    distanz = punkt_A.get_distance_to(punkt_B)
+    zeit_diff = punkt_A.get_time_difference_to(punkt_B)
+    
+    print(f"Punkt A liegt auf Höhe: {punkt_A.ele} m") # Hier nutzen wir den Getter!
+    print(f"Distanz zwischen A und B: {distanz:.2f} Meter")
+    print(f"Zeitunterschied: {zeit_diff} Sekunden")
+    print(f"Geschwindigkeit in diesem Abschnitt: {(distanz/zeit_diff):.2f} m/s")
+    print("-----------------------------\n")
 
 if __name__ == "__main__":
     main()
--- src/models/gps_point.py
+++ src/models/gps_point.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from src.calculations.geo_utils import GeoUtils
 
 class GPSPoint:
     """Repräsentiert einen einzelnen GPS-Messpunkt."""
@@ -17,13 +18,20 @@ class GPSPoint:
         Berechnet die distanz (m) von diesem punkt zu einem anderen GPSPoint.
         """
         # GeoUtils aufrufen
-        pass
-        
+        # (self) übergeben private Variablen von uns selber 
+        # (other) Variablen von einem anderen Punkt an die haversine Formel übergeben
+
+        return GeoUtils.haversine_distance(self._lat, self._lon, other._lat, other._lon)
+
+             
     def get_time_difference_to(self, other: 'GPSPoint') -> float:
         """
         Gibt die Zeitdifferenz in sec. zu einem anderen punkt zurück.
         """
-        pass
+        # .total_seconds() wandelt die datetime-Differenz in echte Sekunden um
+        dt = (other._time - self._time).total_seconds()
+        return abs(dt) # abs() macht die Zeit immer positiv
+
 
     # "Getter", falls ich die Werte mal abfragen möchte
     @property
