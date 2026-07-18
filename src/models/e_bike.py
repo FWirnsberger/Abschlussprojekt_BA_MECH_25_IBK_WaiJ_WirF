d35173b194e9167907bcfb7251c7620f6b903b56
FWirnsberger <wf5817@mci4me.at>
2026-07-18 14:58:08 +0200

    feat: Die Trittleistung des Fahrers wird in EBike Klasse berücksichtigt

--- src/models/e_bike.py
+++ src/models/e_bike.py
@@ -3,15 +3,30 @@ class EBike:
     Model des e-bikes mit seinen physikalischen Eigenschaften.
     """
 
-    def __init__(self, rider_mass: float = 70.0, bike_mass: float = 10.0, 
-                 cw_a: float = 0.5625, wheel_diameter_inch: float = 27.0, motor_constant: float = 1.5):
+    def __init__(self, 
+                 rider_mass: float = 70.0, 
+                 bike_mass: float = 10.0, 
+                 rider_power_w: float = 100,
+                 cw_a: float = 0.5625, 
+                 wheel_diameter_inch: float = 27.0):
         
+        #Fehlererkennung
+        if rider_mass <= 0:
+            raise ValueError("Das Gewicht des Fahrers muss größer 0 sein!")
+        
+        if bike_mass <= 0:
+            raise ValueError("Das Gewicht des Fahrrads muss größer 0 sein!")
+
+        if rider_power_w <= 0:
+            raise ValueError("Die Trittleistung des Fahrers muss größer 0 sein!")
+
         # Öffentliche Attribute 
         self.rider_mass = rider_mass
         self.bike_mass = bike_mass
+        self.rider_power_w = rider_power_w
         self.cw_a = cw_a
         self.wheel_diameter_inch = wheel_diameter_inch
-        self.motor_constant = motor_constant        
+             
         # Für später
         self.battery = None
         self.motor = None
